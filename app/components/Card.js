e76c98c5414349a0977069fe4a6b70b734f7d194
Viktor Recour <viktor@otainsight.com>
2024-02-04 16:56:53 +0100

    use inline SVGs

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,21 +1,8 @@
 import { useEffect, useRef } from 'react'
 
 const Card = ({ children, link }) => {
-  const cardRef = useRef()
-
-  useEffect(() => {
-    const cardRefCurrent = cardRef.current
-
-    const handleClick = () => {
-      window.open(link, '_blank')
-    }
-
-    cardRefCurrent.addEventListener('click', handleClick)
-    return () => cardRefCurrent.removeEventListener('click', handleClick)
-  }, [link])
-
   return (
-    <div ref={cardRef} className='rounded-xl px-6 lg:px-12 py-3 lg:py-6 hover:bg-cyan-900/10 hover:shadow-xl transition-all cursor-pointer group'>
+    <div onClick={() => window.open(link, '_blank')} className='rounded-xl px-6 lg:px-12 py-3 lg:py-6 hover:bg-cyan-900/10 hover:shadow-xl transition-all cursor-pointer group'>
       {children}
     </div>
   );
--- app/components/Project.js
+++ app/components/Project.js
@@ -2,14 +2,26 @@ import TechnologyTag from "./TechnologyTag"
 import ContentfulImage from "./ContenfulImage"
 import { useRef, useEffect } from "react"
 import Card from "./Card"
+import linkIcon from '../../public/link-icon.svg';
+import Image from "next/image";
 
 const Project = ({ project }) => {
-  const { demoLink, repositoryLink, articleLink } = project.fields
+  const { links } = project.fields
 
-  const link = demoLink || repositoryLink || articleLink
+  const cardLink = links[0]
+  const hasMultipleLinks = links && links.length > 1
+
+  const LinkElement = ({ link, text }) => (
+    <a href={link} target='_blank' className='text-slate-200 font-medium hover:text-cyan-200 transition-all' onClick={(e) => e.stopPropagation()}>
+      <span className='flex items-center gap-2 text-xs'>
+        <Image src={linkIcon} alt='Link' width={10} height={10} className='invert' />
+        {text}
+      </span>
+    </a>
+  )
 
   return (
-    <Card link={link}>
+    <Card link={cardLink.fields.link}>
       <div className='flex'>
         <div className='w-1/3 flex justify-center items-center'>     
           {project.fields.images &&
@@ -21,6 +33,16 @@ const Project = ({ project }) => {
           <div className='text-lg text-slate-200 group-hover:text-cyan-200 transition-all'>{project.fields.title}</div>
 
           <div className='text-sm text-slate-400 my-4'>{project.fields.description}</div>
+
+          {hasMultipleLinks &&
+            <div className='flex items-center gap-6'>
+              {links
+                .filter(link => links.indexOf(link) !== 0)
+                .map((link, index) =>
+                <LinkElement key={index} link={link.fields.link} text={link.fields.text} />
+              )}
+            </div>
+          }
 
           <div className="flex flex-wrap gap-1 mt-4">
             {project.fields.technologies &&
